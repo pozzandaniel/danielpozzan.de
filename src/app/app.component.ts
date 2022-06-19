4f6e63256b0767851c087c3637ac20dfb63d19eb
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-06-19 21:08:57 +0200

    added show image function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'danielpozzan.de';
+export class AppComponent implements OnInit{
+
+  images = ['laptop1.jpg', 'laptop2.jpg', 'turin.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+
+  }
+
+  updateImage(){
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(()=>{
+        this.showImage = true;
+      }, 10)
+      
+    }, 8000);
+  }
 }
